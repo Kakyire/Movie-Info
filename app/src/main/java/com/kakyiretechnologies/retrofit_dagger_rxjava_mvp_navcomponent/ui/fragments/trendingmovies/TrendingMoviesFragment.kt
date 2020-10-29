003fda70acb063253864c43da8e6197d44fe32ec
Kakyire <47930771+Kakyire@users.noreply.github.com>
2020-10-29 00:43:09 +0000

    done with implementing fetching of Trending Movies

--- app/src/main/java/com/kakyiretechnologies/retrofit_dagger_rxjava_mvp_navcomponent/ui/fragments/trendingmovies/TrendingMoviesFragment.kt
+++ app/src/main/java/com/kakyiretechnologies/retrofit_dagger_rxjava_mvp_navcomponent/ui/fragments/trendingmovies/TrendingMoviesFragment.kt
@@ -1,6 +1,110 @@
 package com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.ui.fragments.trendingmovies
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.R
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.adapters.MovieAdapter
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.di.component.ActivityComponent
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.di.component.DaggerActivityComponent
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.di.module.FragmentContextModule
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.di.module.AdapterModule
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.di.module.ViewModule
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.model.MovieResults
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.ui.BaseContract
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.utils.BaseApp
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.utils.ClickListener
+import com.kakyiretechnologies.retrofit_dagger_rxjava_mvp_navcomponent.utils.Constants.ERROR
+import kotlinx.android.synthetic.main.trending_movies_fragment.*
+import javax.inject.Inject
 
-class TrendingMoviesFragment:Fragment() {
+class TrendingMoviesFragment : Fragment(R.layout.trending_movies_fragment),
+    TrendingMoviesContract.View, ClickListener {
+    private val TAG = "Results"
+
+    private var movieResults: MutableList<MovieResults> = ArrayList()
+    private var isScrolling = true
+    private var pageNo = 1
+
+    @Inject
+    lateinit var presenter: TrendingMoviesPresenter
+
+    @Inject
+    lateinit var movieAdapter: MovieAdapter
+
+
+    private lateinit var component: ActivityComponent
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        component = DaggerActivityComponent.builder()
+            .fragmentContextModule(FragmentContextModule(this))
+            .adapterModule(context?.let { AdapterModule(this, it) })
+            .viewModule(ViewModule(this))
+            .applicationComponent((context?.applicationContext as BaseApp).getApplicationComponent())
+            .build()
+        component.inject(this)
+        presenter.getMovies()
+        rvTrending.layoutManager = GridLayoutManager(context, 2)
+        rvTrending.adapter = movieAdapter
+
+        scrollListener()
+    }
+
+
+    override fun showProgress() {
+        pbTrending.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        pbTrending.visibility = View.INVISIBLE
+    }
+
+    override fun onFailed(t: Throwable) {
+        Toast.makeText(context, ERROR, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun loadRecyclerView(results: List<MovieResults>) {
+        movieAdapter.loadData(results)
+        movieResults.addAll(results)
+
+        isScrolling = true
+
+        //increase the pageNo so that we load from next page
+        pageNo++
+    }
+
+    override fun onClick(position: Int) {
+        Log.d(TAG, "onClick: ${movieResults[position].title}")
+
+    }
+
+
+    //when the user reach the last item in the recyclerview load more
+    //if the current page is not the last page
+    private fun scrollListener() {
+
+        val layoutManager = rvTrending.layoutManager as GridLayoutManager
+        rvTrending.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val totalCount = layoutManager.itemCount
+                val lastItem = layoutManager.findLastVisibleItemPosition()
+
+
+                if (isScrolling && (lastItem == totalCount - 1)) {
+                    isScrolling = false
+                    presenter.loadMoreMovies(pageNo)
+                }
+
+            }
+        })
+
+    }
+
 }
